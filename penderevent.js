bb5997b17c39d4c096b6dabf7bb96b27cab58ea8
Lorin Beer <lorin.beer@nitobi.com>
2012-05-11 14:27:10 -0700

    initial commit of penderevent.js, halfway between imlpementations

---
+++ penderevent.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright 2012 Adobe Systems Incorporated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+/**
+ * Pender Event
+ *
+ * basic base class definition supporting custom events
+ * written to avoid dependency on a larger library
+ * defines a very basic custom event api
+ * author: Lorin Beer
+ * email : lorin@adobe.com
+ */
+
+
+var PenderEvent = {
+    _listeners : {},
+
+    addListener : function (event,listener) {
+	if (typeof _listeners[event] === "undefined") {
+	    _listeners[event] = [];
+	}
+	_listeners[event].push (listener);
+    },
+
+
+function PenderEvent() {
+    this._eventlisteners = {};
+}
+
+/**
+ * register an event listener
+ */
+prototype.PenderEvent.registerEventListener (eventType,listener) {
+
+}
+
+
+prototype.PenderEvent.fire (eventType, firer) {
+    var i = 0;
+    var listeners = this._eventlisteners[eventType];
+    if ( !(typeof callbacks === "undefined")) {
+	for (i = 0; i < listeners.length; i+=1) {
+	    listeners[i].call (this,eventType);
+	}
+    }
+    else {
+	console.log ("Warning: could find no registered listeners for event type " + eventType );
+    }
+}
